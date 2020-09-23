d9e8e244cb8cce271acd9ac5f26cd50da1decc05
NazmieHasan <58294359+NazmieHasan@users.noreply.github.com>
2020-09-23 15:42:29 +0300

    Trees-Lab

---
+++ Trees-Representation-and-Traversal--BFS-and-DFS-Lab/Tree/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test(15); 
+        }
+
+        public static void Test(int number)
+        {
+            if (number == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine(number);
+            number--;
+            Test(number);
+        }
+    }
+}
