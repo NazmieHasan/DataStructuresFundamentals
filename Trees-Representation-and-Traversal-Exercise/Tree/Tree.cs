51c517d4805cd02887d07170593ed34af16ad84b
NazmieHasan <58294359+NazmieHasan@users.noreply.github.com>
2020-09-24 12:49:40 +0300

    Trees Exercises

---
+++ Trees-Representation-and-Traversal-Exercise/Tree/Tree.cs
@@ -0,0 +1,139 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class Tree<T> : IAbstractTree<T>
+    {
+        private readonly List<Tree<T>> _children;
+
+        public Tree(T key, params Tree<T>[] children)
+        {
+            this.Key = key;
+            this._children = new List<Tree<T>>();
+
+            foreach (var child in children)
+            {
+                this.AddChild(child);
+                child.Parent = this;
+            }
+        }
+
+        public T Key { get; private set; }
+
+        public Tree<T> Parent { get; private set; }
+
+
+        public IReadOnlyCollection<Tree<T>> Children
+            => this._children.AsReadOnly();
+
+        public void AddChild(Tree<T> child)
+        {
+            this._children.Add(child);
+        }
+
+        public void AddParent(Tree<T> parent)
+        {
+            this.Parent = parent;
+        }
+
+        public string GetAsString()
+        {
+            StringBuilder result = new StringBuilder();
+            this.OrderDfsForString(0, result, this);
+
+            return result.ToString().Trim();
+        }
+
+        public Tree<T> GetDeepestLeftomostNode()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<T> GetLeafKeys()
+        {
+            Func<Tree<T>, bool> leafKeysPredicate =
+                (node) => this.IsLeaf(node);
+
+            return this.OrderBfs(leafKeysPredicate);
+        }
+
+        public List<T> GetMiddleKeys()
+        {
+            Func<Tree<T>, bool> middleKeysPredicate
+                = (node) => this.IsMiddle(node);
+
+            return this.OrderBfs(middleKeysPredicate);
+        }
+
+        public List<T> GetLongestPath()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<List<T>> PathsWithGivenSum(int sum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Tree<T>> SubTreesWithGivenSum(int sum)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void OrderDfsForString(int depth,
+            StringBuilder result,
+            Tree<T> subtree)
+        {
+            result
+                .AppendLine(new string(' ', depth) + subtree.Key);
+
+            foreach (var child in subtree.Children)
+            {
+                this.OrderDfsForString(depth + 2, result, child);
+            }
+        }
+
+        private bool IsLeaf(Tree<T> node)
+        {
+            return node.Children.Count == 0;
+        }
+
+        private bool IsRoot(Tree<T> node)
+        {
+            return node.Parent == null;
+        }
+
+        private bool IsMiddle(Tree<T> node)
+        {
+            return node.Parent != null && node.Children.Count > 0;
+        }
+
+        private List<T> OrderBfs(Func<Tree<T>, bool> predicate)
+        {
+            var result = new List<T>();
+            var nodes = new Queue<Tree<T>>();
+
+            nodes.Enqueue(this);
+
+            while (nodes.Count > 0)
+            {
+                var currentNode = nodes.Dequeue();
+
+                if (predicate.Invoke(currentNode))
+                {
+                    result.Add(currentNode.Key);
+                }
+
+                foreach (var child in currentNode.Children)
+                {
+                    nodes.Enqueue(child);
+                }
+            }
+
+            return result;
+        }
+
+    }
+}
